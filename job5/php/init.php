0364ec8fbe281eea435f53f13f03bf7fd60b15ce
PillowSky <ihyzi@163.com>
2014-03-31 23:48:17 +0800

    config for sae

---
+++ job5/php/init.php
@@ -0,0 +1,17 @@
+<?php
+if(defined('SAE_APPNAME')){
+	$con = mysql_connect(SAE_MYSQL_HOST_M.":".SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
+	if (!$con){
+		die(mysql_error());
+	}
+	mysql_query("set character set 'utf8'");
+	mysql_select_db(SAE_MYSQL_DB, $con);
+} else{
+	$con = mysql_connect('localhost','www-data');
+	if (!$con){
+		die(mysql_error());
+	}
+	mysql_query("set character set 'utf8'");
+	mysql_select_db("db2014", $con);
+}
+?>
