377780f02ec6b11c62ef95ccd0a4d3c3a9767147
PillowSky <ihyzi@163.com>
2014-03-31 23:24:28 +0800

    beautify code

--- job5/js/script.js
+++ job5/js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 	//display the student information
 	selectStu();
-	
 });
 /*
 i is the order of a item in the table list,
@@ -10,44 +9,54 @@ all the data are acquired by json[i]
 */
 function selectStu(listID){
 	$.getJSON('php/backend.php', {action: 'select'}, function(json){
-		var content='', i;
+		var content, i;
 		window.json = json;
-		content =	'<tr><th></th><th>学号:</th><th>姓名:</th><th>年龄:</th></tr>';
-		for (i=0; i<json.length; i++) {
-			content += '<tr>';
-			content += '<td><input type="checkbox" value=' + i + '></td>';
-			content += '<td>' + json[i].id + '</td>';
-			content += '<td>' + json[i].name + '</td>';
-			content += '<td>' + json[i].age + '</td>';
-			content += '</tr>';
-		};
+
+		content = '<tr><th></th><th>学号:</th><th>姓名:</th><th>年龄:</th></tr>';
+		if(json.length > 0){
+			for (i=0; i<json.length; i++) {
+				content += '<tr>';
+				content += '<td><input type="checkbox" value=' + i + '></td>';
+				content += '<td>' + json[i].id + '</td>';
+				content += '<td>' + json[i].name + '</td>';
+				content += '<td>' + json[i].age + '</td>';
+				content += '</tr>';
+			};
+		} else{
+			content = '<div class="alert alert-danger">网络好像挂了诶</div>';
+		}
+
 		$('#infoTable').html(content);
 	});
 };
+
 function insertStu(){
-	var insertData = 'action=insert&';
+	var insertData;
+
+	$('#insertResult').remove();
+	
+	insertData = 'action=insert&';
 	insertData += $('#insertInput').serialize();
+	
 	$.get('php/backend.php', insertData, function(data){
 		if(data == '保存成功'){
-			$('#insertResult').fadeOut('fast');
-			$('#insertResult').removeClass("alert-danger");
-			$('#insertResult').addClass("alert-success");
-			$('#insertResult').html(data);
+			$('#insertModal .modal-body').prepend('<div class="alert alert-success" id="insertResult" style="display: none"></div>');
+			$('#insertResult').text(data);
 			$('#insertResult').fadeIn('slow', function() {
 				setTimeout(function(){
 					$('#insertModal').modal('hide');
 					selectStu();
-					$('#insertResult').hide();
+					$('#insertResult').remove();
 				},600);
 			});
 		} else{
-			$('#insertResult').fadeOut('fast');
-			$('#insertResult').addClass("alert-danger");
-			$('#insertResult').html(data);
+			$('#insertModal .modal-body').prepend('<div class="alert alert-danger" id="insertResult" style="display: none"></div>');
+			$('#insertResult').text(data);
 			$('#insertResult').fadeIn('slow');
 		}
 	});
 };
+
 function showUpdateStu(){
 	var json = window.json;
 	var updateArray = $('input:checkbox:checked').map(function (){
@@ -60,7 +69,7 @@ function showUpdateStu(){
 
 	if(updateArray.length == 1){
 		$('#updateModal .btn-warning').show();
-		content += '<input type="text" name="id" class="form-control" disabled="disabled" value="' + json[updateArray[0]].id +'">';
+		content += '<input type="text" name="id" class="form-control" disabled="disabled" value="' + json[updateArray[0]].id + '">';
 		content += '<input type="text" name="name" class="form-control" value="' + json[updateArray[0]].name + '">';
 		content += '<input type="text" name="age" class="form-control" value="' + json[updateArray[0]].age + '">';
 		$('#updateInput').html(content);
@@ -82,15 +91,18 @@ function updateStu(){
 	var updateArray = $('input:checkbox:checked').map(function (){
 		return this.value;
 	}).get();
-	
-	var updateData = 'action=update&id=';
+	var updateData;
+
+	$('#updateResult').remove();
+
+	updateData = 'action=update&id=';
 	updateData += json[updateArray[0]].id + '&';
 	updateData += $('#updateInput').serialize();
 	
 	$.get('php/backend.php', updateData, function(data){
 		if(data == '保存成功'){
 			$('#updateModal .modal-body').prepend('<div class="alert alert-success" id="updateResult" style="display: none"></div>');
-			$('#updateResult').html(data);
+			$('#updateResult').text(data);
 			$('#updateResult').fadeIn('slow', function() {
 				setTimeout(function(){
 					$('#updateModal').modal('hide');
@@ -99,7 +111,7 @@ function updateStu(){
 			});
 		} else{
 			$('#updateModal .modal-body').prepend('<div class="alert alert-danger" id="updateResult" style="display: none"></div>');
-			$('#updateResult').html(data);
+			$('#updateResult').text(data);
 			$('#updateResult').fadeIn('slow');
 		}
 	});
@@ -116,8 +128,12 @@ function showDeleteStu(){
 	}).get();
 	var content = '';
 
-	if(deleteArray.length){
-		$('#deleteModal .alert-danger').text("你真的要删除这些条目了吗？");
+	$('#deleteResult').remove();
+	$('#deleteInput').empty();
+	$('#deleteInput').show();
+
+	if(deleteArray.length > 0){
+		$('#deleteConfirm').text("你真的要删除这些条目了吗？");
 		$('#deleteModal .btn-primary').show();
 
 		content = '<tr><th>学号:</th><th>姓名:</th><th>年龄:</th></tr>'
@@ -128,11 +144,11 @@ function showDeleteStu(){
 			content += '<td>' + json[deleteArray[i]].age + '</td>';
 			content += '</tr>';
 		};
-		$('#deleteTable').html(content);
+
+		$('#deleteInput').html(content);
 	} else{
-		$('#deleteModal .alert-danger').text("什么都没选呀");
+		$('#deleteConfirm').text("什么都没选呀");
 		$('#deleteModal .btn-primary').hide();
-		$('#deleteTable').html('');
 	}
 };
 function deleteStu(){
@@ -141,6 +157,8 @@ function deleteStu(){
 		return this.value;
 	}).get();
 	var errorCount = 0;
+
+	$('#deleteResult').remove();
 
 	for(var i in deleteArray){
 		$.get('php/backend.php', {action: 'delete', id: json[deleteArray[i]].id}, function(data){
@@ -151,16 +169,15 @@ function deleteStu(){
 	}
 	
 	if(errorCount == 0){
-		$('#deleteTable').fadeOut(function() {
-			$('#deleteTable').before('<div class="alert alert-success">所选条目已删除</div>');
-			setTimeout(function(){
+		$('#deleteInput').fadeOut(function() {
+			$('#deleteModal .modal-body').append('<div class="alert alert-success" id="deleteResult" style="display: none">所选条目已删除</div>');
+			$('#deleteResult').fadeIn('slow', function() {
 					$('#deleteModal').modal('hide');
 					selectStu();
-					$("#deleteModal .alert-success").remove();
-					$('#deleteTable').show();
-			},600);
+			});
 		});
 	} else{
-		$('#deleteTable').before('<div class="alert alert-danger">删除失败，请检查输入</div>');
+		$('#deleteModal .modal-body').append('<div class="alert alert-danger" id="deleteResult" style="display: none">删除失败，请检查输入</div>');
+		$('#deleteResult').fadeIn('slow');
 	}
 };
